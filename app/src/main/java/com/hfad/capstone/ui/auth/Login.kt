d10456851ca3fcb44750bf1572e003cbf0df90c8
ronald <ronaltandean@gmail.com>
2021-05-05 10:38:13 +0700

    Prototype Login page (Without UI)

---
+++ app/src/main/java/com/hfad/capstone/ui/auth/Login.kt
@@ -0,0 +1,13 @@
+package com.hfad.capstone.ui.auth
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hfad.capstone.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
