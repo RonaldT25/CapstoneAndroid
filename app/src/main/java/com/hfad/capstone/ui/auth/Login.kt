25c9d9bbd319950a18818938034620abea3a5aea
Randy Yuanda <randyuanda15@gmail.com>
2021-05-18 00:51:44 +0700

    update login and register popup

--- app/src/main/java/com/hfad/capstone/ui/auth/Login.kt
+++ app/src/main/java/com/hfad/capstone/ui/auth/Login.kt
@@ -1,8 +1,10 @@
 package com.hfad.capstone.ui.auth
 
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.hfad.capstone.R
 import com.hfad.capstone.databinding.ActivityLoginBinding
@@ -22,7 +24,33 @@ class Login : AppCompatActivity() {
             startActivity(intent)
         }
     }
+    @SuppressLint("ResourceAsColor", "ResourceType", "UseCompatLoadingForDrawables")
+    private fun loginSuccess() {
+        this.setTheme(R.style.ThemeOverlay_AppCompat_Dark)
+        val builder = AlertDialog.Builder(this)
+        with(builder) {
+            setTitle("Login")
+            setMessage(getText(R.string.login_success))
+            setPositiveButton("OK", null)
+            setIcon(resources.getDrawable(R.drawable.check, theme))
+        }
+        val alertDialog = builder.create()
+        alertDialog.show()
+    }
 
+    @SuppressLint("ResourceAsColor", "ResourceType", "UseCompatLoadingForDrawables")
+    private fun loginFailed() {
+        this.setTheme(R.style.ThemeOverlay_AppCompat_Dark)
+        val builder = AlertDialog.Builder(this)
+        with(builder) {
+            setTitle("Login")
+            setMessage(getText(R.string.login_failed))
+            setPositiveButton("OK", null)
+            setIcon(resources.getDrawable(R.drawable.failed, theme))
+        }
+        val alertDialog = builder.create()
+        alertDialog.show()
+    }
 
 
 
--- app/src/main/java/com/hfad/capstone/ui/auth/Register.kt
+++ app/src/main/java/com/hfad/capstone/ui/auth/Register.kt
@@ -1,8 +1,10 @@
 package com.hfad.capstone.ui.auth
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.hfad.capstone.R
 import com.hfad.capstone.databinding.ActivityLoginBinding
 import com.hfad.capstone.databinding.ActivityRegisterBinding
@@ -20,5 +22,31 @@ class Register : AppCompatActivity() {
             val intent = Intent(this,Login::class.java)
             startActivity(intent)
         }
+    }
+    @SuppressLint("ResourceAsColor", "ResourceType", "UseCompatLoadingForDrawables")
+    private fun registerSuccess() {
+        this.setTheme(R.style.ThemeOverlay_AppCompat_Dark)
+        val builder = AlertDialog.Builder(this)
+        with(builder) {
+            setTitle("Register")
+            setMessage(getText(R.string.register_success))
+            setPositiveButton("OK", null)
+            setIcon(resources.getDrawable(R.drawable.check, theme))
+        }
+        val alertDialog = builder.create()
+        alertDialog.show()
+    }
+    @SuppressLint("ResourceAsColor", "ResourceType", "UseCompatLoadingForDrawables")
+    private fun registerFailed() {
+        this.setTheme(R.style.ThemeOverlay_AppCompat_Dark)
+        val builder = AlertDialog.Builder(this)
+        with(builder) {
+            setTitle("Register")
+            setMessage(getText(R.string.register_failed))
+            setPositiveButton("OK", null)
+            setIcon(resources.getDrawable(R.drawable.failed, theme))
+        }
+        val alertDialog = builder.create()
+        alertDialog.show()
     }
 }
