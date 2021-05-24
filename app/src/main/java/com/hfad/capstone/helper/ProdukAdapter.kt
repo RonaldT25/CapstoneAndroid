8f9435dcb837563c6bfaa3208570315a075c1305
ronald <ronaltandean@gmail.com>
2021-05-24 20:33:56 +0700

    added recyclerview to productTab

---
+++ app/src/main/java/com/hfad/capstone/helper/ProdukAdapter.kt
@@ -0,0 +1,53 @@
+package com.hfad.capstone.helper
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.hfad.capstone.R
+import com.hfad.capstone.data.Product
+import com.hfad.capstone.databinding.ItemListProdukBinding
+import java.util.ArrayList
+
+class ProdukAdapter : RecyclerView.Adapter<ProdukAdapter.ListViewHolder>() {
+
+    private var listData = ArrayList<Product>()
+    var onItemClick: ((Product) -> Unit)? = null
+
+    fun setData(newListData: List<Product>?) {
+        if (newListData == null) return
+        listData.clear()
+        listData.addAll(newListData)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+            ListViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_list_produk, parent, false))
+
+    override fun getItemCount() = listData.size
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val data = listData[position]
+        holder.bind(data)
+    }
+
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = ItemListProdukBinding.bind(itemView)
+        fun bind(data: Product) {
+            with(binding) {
+                Glide.with(itemView.context)
+                        .load(data.image)
+                        .into(ivItemImage)
+                tvItemTitle.text = data.productName
+                tvItemPrice.text = data.price.toString()
+            }
+        }
+
+        init {
+            binding.root.setOnClickListener {
+                onItemClick?.invoke(listData[adapterPosition])
+            }
+        }
+    }
+}
--- app/src/main/java/com/hfad/capstone/ui/produk/tabFragment/ProdukTab.kt
+++ app/src/main/java/com/hfad/capstone/ui/produk/tabFragment/ProdukTab.kt
@@ -5,17 +5,65 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hfad.capstone.R
+import com.hfad.capstone.api.ClientRetrofit
+import com.hfad.capstone.data.Product
 import com.hfad.capstone.databinding.FragmentProdukBinding
 import com.hfad.capstone.databinding.FragmentProdukTabBinding
+import com.hfad.capstone.helper.ProdukAdapter
+import com.hfad.capstone.helper.SessionManager
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProdukTab : Fragment() {
     private var _binding: FragmentProdukTabBinding? = null
     private val binding get() = _binding!!
+    private lateinit var sessionManager: SessionManager
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentProdukTabBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sessionManager = SessionManager(this.requireContext())
+        getProducts()
+    }
+    private fun getProducts(){
+        val produkAdapter = ProdukAdapter()
+
+        sessionManager.fetchAuthToken()?.let {
+            ClientRetrofit.instanceRetrofit.readProduct(it).enqueue(object : Callback<List<Product>> {
+                override fun onResponse(call: Call<List<Product>>, response: Response<List<Product>>) {
+                  val  listProduct = response.body()!!
+                    listProduct?.let {
+                        produkAdapter.onItemClick = { selectedData ->
+                            //        val intent = Intent(activity, DetailMovieActivity::class.java)
+                            //        intent.putExtra(DetailMovieActivity.EXTRA_DATA, selectedData)
+                            //        startActivity(intent)
+                        }
+
+                        produkAdapter.setData(listProduct)
+
+                        with(binding.rvMovie) {
+                            layoutManager = LinearLayoutManager(context)
+                            setHasFixedSize(true)
+                            adapter = produkAdapter
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<List<Product>>, t: Throwable) {
+
+                }
+
+
+            })
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
